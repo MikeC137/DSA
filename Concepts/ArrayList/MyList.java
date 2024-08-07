2aee497d578473ee039b198841168b2573393129
MikeC137 <michalischatzinikolas13@gmail.com>
2024-08-07 00:23:29 -0400

    Add algorithms of each operation in ArrayList class

---
+++ Concepts/ArrayList/MyList.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+
+public interface MyList<E> extends Iterable<E> {
+
+    public boolean add(E element);
+
+    public void add(int index, E element);
+
+    public E remove(int index);
+
+    public boolean remove(E element);
+
+    public E set(int index, E element);
+
+    public E get(int index);
+
+    public boolean contains(E element);
+
+    public boolean equals(Object object);
+
+    public int indexOf(E element);
+
+    public int lastIndexOf(E element);
+
+    public int size();
+
+    public boolean isEmpty();
+
+    public void clear();
+
+    public String toString();
+
+    public Iterator<E> iterator();
+}
