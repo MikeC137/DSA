3a67bd24949aba39da064419396cc8ff5cee658f
MikeC137 <michalischatzinikolas13@gmail.com>
2024-08-23 23:01:44 -0400

    Test 'add' and 'addLast' methods

--- Concepts/MySinglyLinkedList/App.java
+++ Concepts/MySinglyLinkedList/App.java
@@ -4,6 +4,14 @@ public class App {
 
         // Test addFirst(E e)
         linkedList.addFirst("1");
+
+        // Test add(int index, E element)
+        linkedList.add(1, "2");
+        linkedList.add(2, "3");
+
+        // Test addLast(E e)
+        linkedList.addLast("55");
+        linkedList.addLast("77");
         System.out.println(linkedList.toString());
     }
 }
