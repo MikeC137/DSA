e22212bedde942795b845ffafea4b24817f7c864
MikeC137 <michalischatzinikolas13@gmail.com>
2024-08-26 09:58:27 -0400

    Add private fields and create inner class 'Node<E>'

--- Concepts/MyDoublyLinkedList/MyDoublyLinkedList.java
+++ Concepts/MyDoublyLinkedList/MyDoublyLinkedList.java
@@ -1,6 +1,21 @@
 import java.util.Iterator;
 
 public class MyDoublyLinkedList<E> implements MyList<E> {
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+        private Node<E> previous;
+
+        public Node(E data) {
+            this.data = data;
+            next = null;
+            previous = null;
+        }
+
+    }
 
     @Override
     public void addFirst(E e) {
