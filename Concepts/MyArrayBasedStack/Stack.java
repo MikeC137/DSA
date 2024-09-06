38bc268eee586ad2e43b276bbed775505640557d
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-06 09:39:01 -0400

    Create Stack Interface

---
+++ Concepts/MyArrayBasedStack/Stack.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public interface Stack<E> extends Iterable {
+
+    public E push(E item);
+
+    public E peek();
+
+    public E pop();
+
+    public int search(Object o);
+
+    public boolean empty();
+
+    public Iterator<E> iterator();
+}
