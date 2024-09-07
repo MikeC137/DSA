b63d78140e131c04a3fd62dc02f74d4bc0aaeebc
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-07 18:15:43 -0400

    Implement push method in LinkedListStack to add elements to the stack

--- Concepts/MyLinkedListStack/LinkedListStack.java
+++ Concepts/MyLinkedListStack/LinkedListStack.java
@@ -1,4 +1,6 @@
-public class LinkedListStack<E> {
+import java.util.Iterator;
+
+public class LinkedListStack<E> implements Stack<E> {
     private Node<E> top;
     private int size;
 
@@ -12,4 +14,42 @@ public class LinkedListStack<E> {
         }
     }
 
+    @Override
+    public E push(E e) {
+        Node<E> newElement = new Node<>(e);
+        newElement.next = top;
+        top = newElement;
+        size++;
+        return newElement.data;
+    }
+
+    @Override
+    public E peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    @Override
+    public E pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+    @Override
+    public int search(E e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    public boolean emoty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'emoty'");
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
 }
