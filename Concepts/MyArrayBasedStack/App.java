99119ca276838be079aaaa3f92070e68e0e2c04c
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-07 17:27:32 -0400

    Test the following methods in App: push, peek, pop, and search

--- Concepts/MyArrayBasedStack/App.java
+++ Concepts/MyArrayBasedStack/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) {
         ArrayStack<String> stack = new ArrayStack<>();
+
+        // Test E push(E item)
+        stack.push("Mike");
+        stack.push("John");
+        stack.push("Tim");
+        stack.push("Peter");
+        stack.push("Ethan");
+        System.out.println(stack.toString());
+
+        // Test E peek()
+        System.out.println(stack.peek().toString());
+
+        // Test E pop()
+        System.out.println(stack.pop().toString());
+        System.out.println(stack.toString());
+
+        // Test int search(E e)
+        int mikePosition = stack.search("Mike");
+        int timPosition = stack.search("Tim");
+        System.out.println(mikePosition);
+        System.out.println(timPosition);
     }
 }
