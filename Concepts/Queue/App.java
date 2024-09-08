ee3712695e810bf0ee86ffd55521db9bed71ad91
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-08 15:38:27 -0400

    Add code example in App to show how Queues work in LinkedLists

--- Concepts/Queue/App.java
+++ Concepts/Queue/App.java
@@ -1,3 +1,26 @@
-public class App {
+import java.util.Queue;
+import java.util.LinkedList;
 
+public class App {
+    public static void main(String[] args) {
+        // enqueue (add) = offer()
+        // dequeue (remove) = poll()
+
+        // *** Queue is an interface not a class! Therefore, it can't be instantiated.
+        // To utilize queue technology, we need a class that implements Queue interface
+        // LinkedLists and PriorityQueues both implement the Queue interface
+
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer("Karen");
+        queue.offer("Chad");
+        queue.offer("Steve");
+        queue.offer("Harold");
+
+        System.out.println(queue.peek());
+        queue.poll();
+        // If the queue is empty, poll will not throw an exception
+
+        System.out.println(queue);
+    }
 }
