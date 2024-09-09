c0367fbe38aa51ee0ec59333a9a4a50124efebe9
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-09 10:37:29 -0400

    Implement element method in ArrayQueue so that it returns the head of the queue without removing it

--- Concepts/MyArrayQueue/ArrayQueue.java
+++ Concepts/MyArrayQueue/ArrayQueue.java
@@ -1,4 +1,7 @@
-public class ArrayQueue<E> {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayQueue<E> implements Queue<E> {
     private static final int INITIAL_CAPACITY = 10;
     private E[] queue;
     private int front;
@@ -12,4 +15,35 @@ public class ArrayQueue<E> {
         size = 0;
     }
 
+    @Override
+    public E element() {
+        if (empty()) {
+            throw new NoSuchElementException();
+        }
+        return queue[front];
+    }
+
+    @Override
+    public boolean enqueue(E e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'enqueue'");
+    }
+
+    @Override
+    public E peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    @Override
+    public E dequeue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dequeue'");
+    }
+
+    @Override
+    public Iterator iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
 }
