705e18311ec439953864337eab7c28edec1d60b2
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-14 20:47:44 -0400

    Create private Node class with private fields and constructor

--- Concepts/MyLinkedListQueue/LinkedListQueue.java
+++ Concepts/MyLinkedListQueue/LinkedListQueue.java
@@ -1,4 +1,4 @@
-public class LinkedListQueue {
+public class LinkedListQueue<E> {
     private Node<E> head;
     private Node<E> tail;
     private int size;
@@ -7,5 +7,17 @@ public class LinkedListQueue {
         head = null;
         tail = null;
         size = 0;
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+        private Node<E> previous;
+
+        public Node(E data) {
+            this.data = data;
+            next = null;
+            previous = null;
+        }
     }
 }
