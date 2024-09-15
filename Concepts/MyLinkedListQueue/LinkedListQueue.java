f615943cec8e9d5939823f2b6fd7e87d3177d69a
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-14 20:43:07 -0400

    Create LinkedListQueue class with private fields and instructor

---
+++ Concepts/MyLinkedListQueue/LinkedListQueue.java
@@ -0,0 +1,11 @@
+public class LinkedListQueue {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public LinkedListQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+}
