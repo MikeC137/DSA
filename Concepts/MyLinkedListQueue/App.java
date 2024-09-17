ecfc960fa85afccf69c8e20bc14dd2d353b69f15
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-16 21:27:32 -0400

    Test enqueue method to ensure that elements are being added properly in the queue

--- Concepts/MyLinkedListQueue/App.java
+++ Concepts/MyLinkedListQueue/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) {
         LinkedListQueue<String> queue = new LinkedListQueue<>();
+
+        // Test boolean enqueue(E e)
+        queue.enqueue("Mike");
+        queue.enqueue("John");
+        queue.enqueue("Tim");
+        queue.enqueue("Shawn");
+        queue.enqueue("Harold");
+        System.out.println(queue);
+
     }
 }
