03de54a12c3201143cfb1d276c1f5850d6e4a65d
MikeC137 <michalischatzinikolas13@gmail.com>
2024-09-18 23:06:11 -0400

    Create inner class Node and add private fields and constructor

--- Concepts/MyPriorityQueue/MyPriorityQueue.java
+++ Concepts/MyPriorityQueue/MyPriorityQueue.java
@@ -11,6 +11,19 @@ public class MyPriorityQueue<E> implements Queue<E> {
         size = 0;
     }
 
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+        private Node<E> previous;
+
+        public Node(E data) {
+            this.data = data;
+            next = null;
+            previous = null;
+        }
+
+    }
+
     @Override
     public boolean enqueue(E e) {
         // TODO Auto-generated method stub
